9ebfdc8c1314c026e571c183e6bcfcface79c477
cesardmn <cesardmn@gmail.com>
2025-07-10 12:25:31 -0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/vsplayer/sw.js', { scope: '/vsplayer/' })})}
