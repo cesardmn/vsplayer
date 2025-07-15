a25ef4d769284bf3639d20a9d0afc3926d195d32
cesardmn <cesardmn@gmail.com>
2025-07-15 19:59:18 -0300

    feature: criado store do zustand.

--- src/store/playerStore.jsx
+++ src/store/playerStore.jsx
@@ -3,4 +3,12 @@ import { create } from 'zustand'
 export const usePlayer = create((set) => ({
   infoShow: true,
   toggleInfoShow: () => set((state) => ({ infoShow: !state.infoShow })),
+
+  totalFilesToProcess: 0,
+  setTotalFilesToProcess: (count) => set({ totalFilesToProcess: count }),
+  processedFilesCount: 0,
+  setProcessedFilesCount: (count) => set({ processedFilesCount: count }),
+  isProcessing: false,
+  setIsProcessing: (isProcessing) => set({ isProcessing }),
+
 }))
